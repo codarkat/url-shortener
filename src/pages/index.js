1389987d3fbcfe7cfe80cfa8dfc1652f97ec1e98
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-03-29 04:38:08 +0300

    Update UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,73 @@
 import { useState } from "react";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
+
+function AllLinksModal(props) {
+  const { links, baseUrl, ...modalProps } = props;
+  return (
+    <Modal
+      {...modalProps}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      scrollable={true}
+      className="modal-glassmorphism"
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">All Links</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Table striped>
+          <thead>
+            <tr>
+              <th>Alias</th>
+              <th>URL</th>
+            </tr>
+          </thead>
+          <tbody>
+            {links.map((link) => (
+              <tr key={link.alias}>
+                <td>
+                  <a
+                    href={baseUrl.concat("/", link.alias)}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {baseUrl.concat("/", link.alias)}
+                  </a>
+                </td>
+                <td>
+                  <a href={link.url} target="_blank" rel="noopener noreferrer">
+                    {link.url}
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 export default function Home({ links }) {
   const [url, setUrl] = useState("");
+  const [baseUrl, setBaseUrl] = useState("");
   const [customAlias, setCustomAlias] = useState("");
   const [shortUrl, setShortUrl] = useState("");
   const [error, setError] = useState("");
   const [allLinks, setAllLinks] = useState(links);
+  const [modalShow, setModalShow] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   async function handleSubmit(event) {
     event.preventDefault();
+    setLoading(true);
     setError("");
     setShortUrl("");
     const response = await fetch("/api/shortener", {
@@ -26,72 +83,143 @@ export default function Home({ links }) {
       const errorData = await response.json();
       setError(errorData.error);
     }
+    setLoading(false);
   }
 
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>URL Shortener</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>URL Shortener</h1>
-        <form onSubmit={handleSubmit}>
-          <label className={styles.label}>
-            URL:
-            <input
-              className={styles.input}
-              type="url"
-              required
-              value={url}
-              onChange={(event) => setUrl(event.target.value)}
-            />
-          </label>
-          <label className={styles.label}>
-            Custom Alias (optional):
-            <input
-              className={styles.input}
-              type="text"
-              value={customAlias}
-              onChange={(event) => setCustomAlias(event.target.value)}
-            />
-          </label>
-          <button className={styles.button} type="submit">
-            Shorten
-          </button>
-        </form>
-        {error && <p className={styles.error}>{error}</p>}
-        {shortUrl && (
-          <p className={styles.shortUrl}>
-            <a href={shortUrl} target="_blank" rel="noopener noreferrer">
-              {shortUrl}
-            </a>
-          </p>
-        )}
-        <h2 className={styles.subtitle}>All Links</h2>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>Alias</th>
-              <th>URL</th>
-            </tr>
-          </thead>
-          <tbody>
-            {allLinks.map((link) => (
-              <tr key={link.alias}>
-                <td>{link.alias}</td>
-                <td>
-                  <a href={link.url} target="_blank" rel="noopener noreferrer">
-                    {link.url}
-                  </a>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div
+        className="position-absolute w-100 h-100"
+        style={{
+          backgroundImage: `url("https://picsum.photos/1920/1080")`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          opacity: "0.8",
+          zIndex: "-1",
+        }}
+      >
+        <div
+          className="position-absolute w-100 h-100 bg-dark"
+          style={{ opacity: "0.2" }}
+        />
+      </div>
+
+      <main className="position-absolute w-100 h-100 d-flex justify-content-center align-items-center">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-6 col-lg-5">
+              <div className="card shadow-lg p-3 bg-glassmorphism">
+                <div className="card-body text-center">
+                  <h1 className="h2 mb-4 fw-bold text-uppercase text-white">
+                    Shorten your URL
+                  </h1>
+                  {error && <div className="alert alert-danger">{error}</div>}
+                  {shortUrl && (
+                    <div className="alert alert-success">
+                      <a
+                        href={shortUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {shortUrl}
+                      </a>
+                    </div>
+                  )}
+                  <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                      <label
+                        htmlFor="url"
+                        className="form-label visually-hidden"
+                      >
+                        URL
+                      </label>
+                      <input
+                        type="url"
+                        name="url"
+                        id="url"
+                        className="form-control"
+                        placeholder="Enter your URL"
+                        value={url}
+                        onChange={(event) => setUrl(event.target.value)}
+                        required
+                        autoFocus
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label
+                        htmlFor="alias"
+                        className="form-label visually-hidden"
+                      >
+                        Custom alias (optional)
+                      </label>
+                      <input
+                        type="text"
+                        name="alias"
+                        id="alias"
+                        className="form-control"
+                        placeholder="Custom alias (optional)"
+                        value={customAlias}
+                        onChange={(event) => setCustomAlias(event.target.value)}
+                      />
+                    </div>
+                    <div className="d-grid">
+                      {loading ? (
+                        <>
+                          <Button disabled variant="secondary">
+                            Loading
+                          </Button>
+                        </>
+                      ) : (
+                        <>
+                          <Button type="submit" variant="primary">
+                            Shorten
+                          </Button>
+                          <Button
+                            variant="primary"
+                            className="mt-3"
+                            onClick={() => {
+                              setModalShow(true);
+                              setBaseUrl(window.location.origin);
+                            }}
+                          >
+                            All links
+                          </Button>
+                        </>
+                      )}
+                      {/* <button type="submit" className="btn btn-primary">
+                        Shorten
+                      </button>
+                      <Button
+                        variant="primary"
+                        className="mt-3"
+                        onClick={() => {
+                          setModalShow(true);
+                          setBaseUrl(window.location.origin);
+                        }}
+                      >
+                        All links
+                      </Button> */}
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
-    </div>
+
+      <AllLinksModal
+        show={modalShow}
+        links={allLinks}
+        baseUrl={baseUrl}
+        onHide={() => setModalShow(false)}
+      />
+    </>
   );
 }
 
